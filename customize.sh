665087cc921ce72e502f7366c9f1c8a5bf28c89c
lululoid <ereiwn@gmail.com>
2023-08-22 08:57:27 +0700

    BREAKING CHANGES!: remove options for installation without signature verification disabled

--- customize.sh
+++ customize.sh
@@ -1,3 +1,4 @@
+# shellcheck disable=SC2034,SC2086,SC3010
 SKIPUNZIP=1
 SKIPMOUNT=false
 android=$(getprop ro.build.version.release)
@@ -21,8 +22,6 @@ _ui_print() {
 }
 
 install_files() {
-	. "$MODPATH"/addon/install.sh
-
 	ui_print " READ!!! "
 	ui_print " Signature verification must be disabled"
 	ui_print " mandatory for MIUI 14 users based on"
@@ -48,16 +47,16 @@ install_files() {
 	if [ -z "$launcher_dir" ]; then
 		_ui_print "> Installing on non MIUI system?"
 
-		if [ "$android" = 11 ] || [ "$android" = 12 ]; then
-			REPLACE="/system/priv-app/aMiuiHome"
+		if [ $android -eq 11 ] || [ $android -eq 12 ]; then
+			REPLACE=/system/priv-app/aMiuiHome
 
 			ui_print "> Android < 13 detected"
-			cp -rf "$MODPATH/files/launcher/MiuiHome.apk" "$MODPATH$REPLACE"
-		elif [ "$android" = 13 ]; then
-			REPLACE="/system/product/priv-app/aMiuiHome"
+			cp -rf $MODPATH/files/launcher/MiuiHome.apk "$MODPATH$REPLACE"
+		elif [ $android -eq 13 ]; then
+			REPLACE=/system/product/priv-app/aMiuiHome
 
 			ui_print "> Android 13 detected"
-			cp -rf "$MODPATH/files/launcher/MiuiHome.apk" "$MODPATH$REPLACE"
+			cp -rf $MODPATH/files/launcher/MiuiHome.apk "$MODPATH$REPLACE"
 		else
 			_ui_print "> Version not supported. Please upgrade your system."
 			abort " Aborting..."
@@ -65,56 +64,48 @@ install_files() {
 	fi
 
 	ui_print "> Uninstalling launcher updates.."
-	pm uninstall-system-updates com.miui.home 1>/dev/null && 
-    ui_print " Launcher updates uninstalled"
-	_ui_print "> Choose your MIUI Version:"
-	ui_print "  Vol+ = Android 13 or MIUI 14 CN"
-	ui_print "  Vol- = MIUI 14 or < A13"
+	pm uninstall-system-updates com.miui.home 1>/dev/null &&
+		ui_print " Launcher updates uninstalled"
+	installation=$(pm install "$MODPATH"/files/launcher/MiuiHome.apk)
 
-	# installation
-	if chooseport; then
-		installation=$(pm install "$MODPATH"/files/launcher/MiuiHome.apk)
+	log_it "installation=$(pm install "$MODPATH"/files/launcher/MiuiHome.apk)"
+	log_it "$(ls "$MODPATH"/files/launcher/)"
 
-		log_it "installation=$(pm install "$MODPATH"/files/launcher/MiuiHome.apk)"
-		log_it "$(ls "$MODPATH"/files/launcher/)"
+	[[ "$installation" != "Success" ]] &&
+		abort "> Please Disable signature verification"
 
-		[[ "$installation" != "Success" ]] &&
-			abort "> Please Disable signature verification"
-
-		mv "$MODPATH"/files/launcher/MiuiHome.apk "$MODPATH$launcher"
-	else
-		mv "$MODPATH"/files/launcher/MiuiHome.apk "$MODPATH$launcher"
-	fi
-
-	if [ "$installation" = "Success" ]; then
-		ui_print "> Miui launcher installation success, no need to reboot"
-	else
-		ui_print "> MIUI launcher mod installed but please reboot first"
-	fi
+	mv "$MODPATH"/files/launcher/MiuiHome.apk "$MODPATH$launcher"
+	[ "$installation" = "Success" ] &&
+		ui_print "> Miui launcher installation success, you could check it out now."
 
 	is_poco=$(find /system/product/overlay -type f -iname "Framework_resoverlay.apk")
 	[ -n "$is_poco" ] && {
 		_ui_print "> POCO detected"
-		mkdir "$MODPATH"/system/product/overlay
-		cp -rf "$MODPATH"/files/poco/Framework_resoverlay.apk "$MODPATH"/system/product/overlay
+		mkdir $MODPATH/system/product/overlay
+		cp -rf $MODPATH/files/poco/Framework_resoverlay.apk $MODPATH/system/product/overlay
 	}
 }
 
 set_monet() {
-	. "$MODPATH"/addon/install.sh
-
-	[ "$android" -gt 11 ] && {
+	[ $android -gt 11 ] && {
 		ui_print " Do you want Monet colors?"
 		ui_print "  Vol+ = Yes"
 		ui_print "  Vol- = No"
 
-		if chooseport; then
-			mkdir -p "$MODPATH/system/product/overlay"
-			cp -rf "$MODPATH/files/MonetMiuiHome.apk" "$MODPATH/system/product/overlay"
-			ui_print "> Monet installed"
-		else
-			ui_print "> Monet is cool ynow?"
-		fi
+		while true; do
+			# shellcheck disable=SC2069
+			timeout 0.25 /system/bin/getevent -lqc 1 2>&1 >"$TMPDIR"/events &
+			sleep 0.1
+			if (grep -q 'KEY_VOLUMEUP *DOWN' "$TMPDIR"/events); then
+				mkdir -p "$MODPATH/system/product/overlay"
+				cp -rf "$MODPATH/files/MonetMiuiHome.apk" "$MODPATH/system/product/overlay"
+				ui_print "> Monet installed"
+				break
+			elif (grep -q 'KEY_VOLUMEDOWN *DOWN' "$TMPDIR"/events); then
+				ui_print "> Monet is cool ynow?"
+				break
+			fi
+		done
 	}
 }
 
@@ -124,9 +115,8 @@ set_permissions() {
 }
 
 cleanup() {
-	rm -rf "$MODPATH"/addon 2>/dev/null
-	rm -rf "$MODPATH"/files 2>/dev/null
-	rm -f "$MODPATH"/install.sh 2>/dev/null
+	rm -rf $MODPATH/files 2>/dev/null
+	rm -f $MODPATH/install.sh 2>/dev/null
 	ui_print "> Deleting package cache files"
 	rm -rf /data/resource-cache/*
 	rm -rf /data/system/package_cache/*
@@ -136,17 +126,13 @@ cleanup() {
 	touch /data/adb/modules/miui_launcher_mod/remove
 }
 
-run_install() {
-	unzip -o "$ZIPFILE" -x 'META-INF/*' -d "$MODPATH" >&2
-	ui_print "> Installing files"
-	set_permissions
-	install_files
-	set_monet
-	sleep 1
-	ui_print "> Cleaning up"
-	cleanup
-	sleep 1
-	ui_print "> Removing any MIUI Launcher folder to avoid issues"
-}
-
-run_install
+unzip -o $ZIPFILE -x 'META-INF/*' -d $MODPATH >&2
+ui_print "> Installing files"
+set_permissions
+install_files
+set_monet
+sleep 1
+ui_print "> Cleaning up"
+cleanup
+sleep 1
+ui_print "> Removing any MIUI Launcher folder to avoid issues"
